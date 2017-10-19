63fed0340f4f97b87a6accbf6350166698e65420
YifengGuo <gyf940517@gmail.com>
2017-10-18 23:19:35 -0400

    fetch and store content from HTML in JSON format

--- HTML_storage_no_database/store_HTML_as_JSON.py
+++ HTML_storage_no_database/store_HTML_as_JSON.py
@@ -1,19 +1,29 @@
+# coding:utf-8
 # precondition is the web is static
 # no content is loaded by JavaScript dynamically
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8') # make sure content is encoded by utf-8 instead of ascii which cannot represent Chinese
+import json
 import requests
 from bs4 import BeautifulSoup
 
 user_agent = 'Mozzila/4.0 (compatible; MISE 5.5; Windows NT)'
 headers = {'User-Agent':user_agent}
 r = requests.get('http://seputu.com', headers=headers)
-print r.text
+print r.text # print html
 
-soup = BeautifulSoup(r.text, 'html.parser',from_encoding='utf-8')
-for mulu in soup.find_all(class_="mulu"):
+soup = BeautifulSoup(r.text, 'html.parser')
+content = []
+for mulu in soup.find_all(class_="mulu"):   # type of mulu: bs4.element.Tag (list)
     h2 = mulu.find('h2')
     if h2 != None:
         h2_title = h2.string # obtain title
+        list = []
         for a in mulu.find(class_='box').find_all('a'):
             href = a.get('href')
             box_title = a.get('title')
-            print href,box_title
+            list.append({'href':href, 'box_title':box_title})
+        content.append({'title':h2_title,'content':list})
+with open('daomubiji.json','wb') as fp:
+    json.dump(content,fp=fp,indent=4,ensure_ascii=False)
